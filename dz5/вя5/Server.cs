1e93df6f7be836fa5d35bac84e83d8972dcc04b1
IvanNklv <nikolaevivan229@gmail.com>
2025-09-19 10:01:32 +0300

    Add files via upload

---
+++ dz5/вя5/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    static List<TcpClient> clients = new List<TcpClient>();
+
+    static void Main()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Сервер запущено на порту 5000...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            clients.Add(client);
+            Console.WriteLine("Клієнт підключився.");
+
+            Thread t = new Thread(HandleClient);
+            t.Start(client);
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int byteCount;
+
+        while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+        {
+            string msg = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            Console.WriteLine("Отримано: " + msg);
+            Broadcast(msg, client);
+        }
+
+        Console.WriteLine("Клієнт відключився.");
+        clients.Remove(client);
+        client.Close();
+    }
+
+    static void Broadcast(string msg, TcpClient sender)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(msg);
+        foreach (var c in clients)
+        {
+            if (c != sender)
+            {
+                c.GetStream().Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
---
+++ dz5/вя5/client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Client
+{
+    static void Main()
+    {
+        Console.Write("Введіть ваш нік: ");
+        string name = Console.ReadLine();
+
+        TcpClient client = new TcpClient("127.0.0.1", 5000);
+        NetworkStream stream = client.GetStream();
+
+        Thread receiveThread = new Thread(() =>
+        {
+            byte[] buffer = new byte[1024];
+            int byteCount;
+            while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string msg = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                Console.WriteLine(msg);
+            }
+        });
+        receiveThread.Start();
+
+        Console.WriteLine("Ви підключені. Напишіть /exit для виходу.");
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            if (message.ToLower() == "/exit")
+                break;
+
+            byte[] buffer = Encoding.UTF8.GetBytes($"{name}: {message}");
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        client.Close();
+    }
+}
